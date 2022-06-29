fe4ca1dfeea44d897f058cdfc7b734ef3397d819
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-29 11:19:27 +0200

    feat(room): create database

--- app/src/main/java/com/example/forage/data/ForageDatabase.kt
+++ app/src/main/java/com/example/forage/data/ForageDatabase.kt
@@ -15,6 +15,8 @@
  */
 package com.example.forage.data
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.forage.model.Forageable
 
@@ -23,4 +25,23 @@ import com.example.forage.model.Forageable
  * This database stores a [Forageable] entity
  */
 // TODO: create the database with all necessary annotations, methods, variables, etc.
-abstract class ForageDatabase : RoomDatabase()
+abstract class ForageDatabase : RoomDatabase() {
+    abstract fun forageableDao(): ForageableDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ForageDatabase? = null
+        fun getDatabase(context: Context): ForageDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ForageDatabase::class.java, "forageable_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
